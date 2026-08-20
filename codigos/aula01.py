f1dc97876b7590ac3a22f21fadeae3f2f4fe77cb
Luiz Jardel <luizjardel13@gmail.com>
2026-08-20 20:29:07 -0300

    Códigos Atualizados

---
+++ codigos/aula01.py
@@ -0,0 +1,10 @@
+inteiro=int(input("Digite sua idade: "))
+nome = input("Digite seu nome: ")
+altura = float(input("Digite sua altura:"))
+print("Seu nome é:",nome)
+print("Sua idade é:", inteiro)
+print("Sua altura é:", altura)
+
+##teste de códigos na primeira aula lendo apenas documentação do python :D
+##Java>>Python apenas
+##Springboot >> FastAPI , Django e Flask
