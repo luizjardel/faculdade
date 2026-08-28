a2e82e43159b966f498dc2ea348de9f4591ddb6a
dev-jardel13 <luizjardel13@gmail.com>
2026-08-27 22:51:48 -0300

    codigos atualizados

---
+++ codigos/dicionario.py
@@ -0,0 +1,24 @@
+##Tarefa de casa dia 27/08/2026
+##Professor Abrãoo
+aluno = {
+    "nome": "Jardel",
+    "idade": 20,
+    "curso": "Análise e Desenvolvimento de Sistemas",
+    "semestre": "4",
+    "email": "luizjardel13@gmail.com"
+    
+}
+print(aluno['nome'])
+print(aluno['idade'])
+print(aluno['curso'])
+print(aluno['semestre'])
+print (type(aluno))
+
+aluno["semestre"] = "5"
+print(aluno)
+
+aluno["nota"] = 7.0
+
+print(aluno['nome'])
+print(aluno['curso'])
+print(aluno['nota'])
--- codigos/tetris.c
+++ codigos/tetris.c
@@ -19,6 +19,7 @@ typedef struct {
 } Fila;
 
 
+
 int contadorID = 0;
 
 
